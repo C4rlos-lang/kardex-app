e9273e1ca293311bda789202a99d91a7fbc3b781
Carlos_sosa <carlosasosam1@gmail.com>
2026-03-30 16:49:32 -0500

    fix cors para produccion

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
 
 # ── 1. Configurar la base de datos ────────────────────────────────
-engine = create_engine("sqlite:///./productos.db", connect_args={"check_same_thread": False})
+engine = create_engine("postgresql://postgres:C4rlossosa.1990@db.thcdadlejpdhaaekyost.supabase.co:5432/postgres")
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
